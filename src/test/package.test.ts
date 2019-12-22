9cb16b014833fb715bcfa357afbcfc25b9b637d2
Gabriel Paul "Cley Faye" Risterucci <cleyfaye@gmail.com>
2019-12-23 00:07:00 +0100

    Some cleanup

--- src/test/package.test.ts
+++ src/test/package.test.ts
@@ -11,30 +11,32 @@ const packageData = {
 const moveToTestDir = (testDir: string): void =>
   process.chdir(`testdata/${testDir}`);
 
-describe("readFromPackage()", function() {
-  let initialCwd: string;
-  before(function() {
-    initialCwd = process.cwd();
-  });
-  beforeEach(function() {
-    process.chdir(initialCwd);
-  });
-  after(function() {
-    process.chdir(initialCwd);
-  });
-  it("Get expected values from package.json", async function() {
-    moveToTestDir("havevalue");
-    const readData = await readFromPackage("testPkg");
-    readData.should.eql(packageData);
-  });
-  it("Get empty if section is missing", async function() {
-    moveToTestDir("nothavevalue");
-    const readData = await readFromPackage("testPkg");
-    readData.should.eql({});
-  });
-  it("Get empty if file does not exist", async function() {
-    moveToTestDir("nofile");
-    const readData = await readFromPackage("testPkg");
-    readData.should.eql({});
+describe("package", function() {
+  describe("readFromPackage()", function() {
+    let initialCwd: string;
+    before(function() {
+      initialCwd = process.cwd();
+    });
+    beforeEach(function() {
+      process.chdir(initialCwd);
+    });
+    after(function() {
+      process.chdir(initialCwd);
+    });
+    it("Get expected values from package.json", async function() {
+      moveToTestDir("havevalue");
+      const readData = await readFromPackage("testPkg");
+      readData.should.eql(packageData);
+    });
+    it("Get empty if section is missing", async function() {
+      moveToTestDir("nothavevalue");
+      const readData = await readFromPackage("testPkg");
+      readData.should.eql({});
+    });
+    it("Get empty if file does not exist", async function() {
+      moveToTestDir("nofile");
+      const readData = await readFromPackage("testPkg");
+      readData.should.eql({});
+    });
   });
 });
