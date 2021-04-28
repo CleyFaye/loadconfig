bd3d99451205db963007ba82b73be5c69f78112a
Gabriel Paul 'Cley Faye' Risterucci <gabriel@keeex.net>
2021-04-28 15:09:00 +0200

    Added tests related to latest features

--- src/test/loader.test.ts
+++ src/test/loader.test.ts
@@ -1,5 +1,5 @@
 import "chai/register-should";
-import loadConfig, {readFromDefaultValues, clearCache} from "../loader";
+import loadConfig, {readFromDefaultValues, clearCache, DataSource} from "../loader";
 import {
   options,
   defaultData,
@@ -46,61 +46,86 @@ describe("loader", function() {
     it("Get combined values (cli)", function() {
       moveToTestDir("havevalue");
       replaceArgv(cliArgs);
-      const readData = loadConfig(options, configName);
+      const readData = loadConfig({options}, configName);
       readData.should.eql(cliData);
     });
     it("Get combined values (js)", function() {
       moveToTestDir("havevalue");
       replaceArgv(["--somethingElse"]);
-      const readData = loadConfig(options, configName);
+      const readData = loadConfig({options}, configName);
       readData.should.eql({
         ...jsData,
         boolOpt: false,
       });
     });
+    it("Get combined values (skip json)", function() {
+      moveToTestDir("havevaluejson");
+      replaceArgv(["--somethingElse"]);
+      const readData = loadConfig({
+        options,
+        disableSource: new Set([DataSource.json]),
+      }, configName);
+      readData.should.eql(packageData);
+    });
     it("Get combined values (json)", function() {
       moveToTestDir("havevaluejson");
       replaceArgv(["--somethingElse"]);
-      const readData = loadConfig(options, configName);
+      const readData = loadConfig({options}, configName);
+      readData.should.eql(jsonData);
+    });
+    it("Get combined values (json no dot)", function() {
+      moveToTestDir("havevaluejsonnodot");
+      replaceArgv(["--somethingElse"]);
+      const readData = loadConfig(
+        {
+          options,
+          noDotFile: true,
+        },
+        configName,
+      );
       readData.should.eql(jsonData);
     });
     it("Get combined values (package.json)", function() {
       moveToTestDir("havevaluepackage");
       replaceArgv(["--somethingElse"]);
-      const readData = loadConfig(options, configName);
+      const readData = loadConfig({options}, configName);
       readData.should.eql(packageData);
     });
     it("Get combined values (defaults)", function() {
       moveToTestDir("nothavevalue");
       replaceArgv(["--somethingElse"]);
-      const readData = loadConfig(options, configName);
+      const readData = loadConfig({options}, configName);
       readData.should.eql(defaultData);
     });
     it("Get combined values (no external files, cli)", function() {
       moveToTestDir("nothavevalue");
       replaceArgv(cliArgs);
-      const readData = loadConfig(options);
+      const readData = loadConfig({options});
       readData.should.eql(cliData);
     });
     it("Get combined values (no external files, defaults)", function() {
       moveToTestDir("nothavevalue");
       replaceArgv(["--somethingElse"]);
-      const readData = loadConfig(options);
+      const readData = loadConfig({options});
       readData.should.eql(defaultData);
     });
     it("Test cache", function() {
       moveToTestDir("nofile");
       replaceArgv(["--str", "argStr"]);
       const readData = loadConfig({
-        str: {
-          type: OptionType.STRING,
+        options: {
+          str: {
+            type: OptionType.STRING,
+          },
         },
       });
       readData.should.eql({str: "argStr"});
       // Command line should be exhausted by now
       const readData2 = loadConfig({
-        str: {
-          type: OptionType.STRING,
+        options: {
+          str: {
+            type: OptionType.STRING,
+          },
         },
       });
       readData2.should.eql({str: "argStr"});
